8e1b2be637e735396ce29b48e0c97728f6930092
faisalapt <faisal.aprianto@student.umn.ac.id>
2020-11-22 11:40:51 +0700

    profile

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './profile.jpg';
 import './App.css';
+import { Button, Card } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fab)
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Card bg="dark" style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={logo} />
+        <Card.Body>
+          <Card.Title style={{color:'black'}}>Profile</Card.Title>
+          <Card.Text style={{color:'black'}}>
+            Informatika - 2019
+            <h1><FontAwesomeIcon icon={['fab','github']}/><FontAwesomeIcon icon={['fab','instagram']}/></h1>
+          </Card.Text>
+          <Button variant="primary"></Button>
+        </Card.Body>
+        </Card>
+        <Button>Test Button</Button>
       </header>
     </div>
   );
