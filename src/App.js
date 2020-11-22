51ce5b37f4abe8394d190fec850078c6111d912a
faisalapt <faisal.aprianto@student.umn.ac.id>
2020-11-22 14:27:57 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import logo from './profile.jpg';
+import foto from './profile.jpg';
+import vue from './vue.png';
+import angular from './AngularJS.svg'
+import logoreact from './logo192.png'
 import './App.css';
-import { Button, Card } from 'react-bootstrap'
+import { Button, Card, CardColumns, ProgressBar } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -13,17 +16,45 @@ function App() {
     <div className="App">
       <header className="App-header">
       <Card bg="dark" style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={logo} />
+        <Card.Img variant="top" src={foto} />
+        <Card.Header style={{color:'black'}}>Profile</Card.Header>
         <Card.Body>
-          <Card.Title style={{color:'black'}}>Profile</Card.Title>
           <Card.Text style={{color:'black'}}>
+            Faisal Aprianto
+            00000042520
             Informatika - 2019
-            <h1><FontAwesomeIcon icon={['fab','github']}/><FontAwesomeIcon icon={['fab','instagram']}/></h1>
           </Card.Text>
-          <Button variant="primary"></Button>
+          <Button variant="dark" size="lg" onClick={(e) => {
+            e.preventDefault()
+            window.open('https://github.com/faisalapt')
+          }}><FontAwesomeIcon icon={['fab','github']}/></Button>
+          <Button variant="dark" size="lg" onClick={(e) => {
+            e.preventDefault()
+            window.open('https://www.instagram.com/faisalapt/')
+          }}><FontAwesomeIcon icon={['fab','instagram']}/></Button>
         </Card.Body>
         </Card>
-        <Button>Test Button</Button>
+        <h2>Experience</h2>
+        <div className="card mb-3" style={{width:'500px'}}>
+        </div>
+        <CardColumns>
+          <Card bg="dark" style={{width:'18rem',marginLeft:'30px'}}>
+            <Card.Img variant="bottom" src={vue}/>
+            <Card.Header>VueJS</Card.Header>
+            <Card.Body>
+              <Card.Text>Progress</Card.Text>
+              <ProgressBar animated now={20}/>
+            </Card.Body>
+          </Card>
+          <Card bg="dark" style={{width:'18rem',marginLeft:'20px'}}>
+            <Card.Img variant="bottom" src={logoreact}/>
+            <Card.Header>ReactJS</Card.Header>
+            <Card.Body>
+              <Card.Text>Progress</Card.Text>
+              <ProgressBar animated now={15}/>
+            </Card.Body>
+          </Card>
+        </CardColumns>
       </header>
     </div>
   );
